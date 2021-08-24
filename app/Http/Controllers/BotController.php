19a2e40aeefb529eaca81fb6b485b8a15538a19f
Derrick Mwema <derrickmbugua50@gmail.com>
2021-08-25 02:02:15 +0300

    feat: send message

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use Telegram\Bot\Api;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class BotController extends Controller
 {
-    //
+    public function index()
+    {
+        $telegram = new Api(env('TELEGRAM_BOT_TOKEN'));
+        $response = $telegram->getMe();
+        Log::info("Telegram Response: " . $response);
+        $botId = $response->getId();
+        $firstName = $response->getFirstName();
+        $username = $response->getUsername();
+
+        Log::info("Send message");
+
+        $response = $telegram->sendMessage([
+            'chat_id' => 1379510697,
+            'text' => "Hello world!",
+        ]);
+
+        $messageId = $response->getMessageId();
+    }
 }
